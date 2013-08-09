3169a036a2b627d390d812370a75c4890409368e
Joseph Rodriguez <joerodriguez@gmail.com>
2013-08-09 12:27:37 -0400

    action controller request driver should also use new header key

--- lib/api_auth/request_drivers/action_controller.rb
+++ lib/api_auth/request_drivers/action_controller.rb
@@ -13,7 +13,7 @@ module ApiAuth
       end
 
       def set_auth_header(header)
-        @request.env["Authorization"] = header
+        @request.env["HMAC_AUTH"] = header
         @headers = fetch_headers
         @request
       end
@@ -69,7 +69,7 @@ module ApiAuth
       end
 
       def authorization_header
-        find_header %w(Authorization AUTHORIZATION HTTP_AUTHORIZATION)
+        find_header %w(HMAC_AUTH AUTHORIZATION HTTP_AUTHORIZATION)
       end
 
     private
